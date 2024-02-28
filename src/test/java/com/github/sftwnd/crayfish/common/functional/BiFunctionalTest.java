7b4b7b233d57baf8cb932481b320093b05f1261d
Andrey Shindarev <ashindarev@gmail.com>
2024-02-29 00:23:40 +0300

    Functional extended methods has been realized (with tests)

--- src/test/java/com/github/sftwnd/crayfish/common/functional/BiFunctionalTest.java
+++ src/test/java/com/github/sftwnd/crayfish/common/functional/BiFunctionalTest.java
@@ -29,6 +29,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 class BiFunctionalTest {
 
     @Test
--- src/test/java/com/github/sftwnd/crayfish/common/functional/FunctionalTest.java
+++ src/test/java/com/github/sftwnd/crayfish/common/functional/FunctionalTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static com.github.sftwnd.crayfish.common.functional.Functional.cast;
 import static com.github.sftwnd.crayfish.common.functional.Functional.functional;
@@ -20,8 +22,10 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -73,6 +77,70 @@ class FunctionalTest {
         Processable processable = functional.processable(parameter);
         assertNull(processable.call(), "Functional.processable(value).call has to return null");
         verify(function, times(1)).apply(parameter);
+    }
+    
+    @Test
+    void furtherRunTest() {
+        var runnable = mock(Runnable.class);
+        doNothing().when(runnable).run();
+        verify(runnable, never()).run();
+        assertSame(this.result, function.furtherRun(runnable::run).apply(parameter), "Functional::furtherRun(processable) has to return right result");
+        verify(runnable, times(1)).run();
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void furtherAcceptTest() {
+        var consumable = mock(Consumable.class);
+        doNothing().when(consumable).accept(any());
+        verify(consumable, never()).accept(any());
+        assertSame(this.result, function.furtherAccept(consumable::accept).apply(parameter), "Functional::furtherAccept(consumable) has to return right result");
+        verify(consumable, times(1)).accept(result);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void furtherApplyTest() {
+        Function<Object, Integer> function = mock(Function.class);
+        when(function.apply(result)).thenReturn(randomValue);
+        verify(function, never()).apply(any());
+        assertSame(randomValue, this.function.furtherApply(function::apply).apply(parameter), "Functional::andThen(functional) has to return right result");
+        verify(function, times(1)).apply(result);
+    }
+
+    @Test
+    void previouslyTest() {
+        var mock = mock(Runnable.class);
+        doNothing().when(mock).run();
+        Runnable runnable = () -> {
+            verify(this.function, never()).apply(any());
+            mock.run();
+        };
+        verify(mock, never()).run();
+        assertSame(this.result, function.previously(runnable::run).apply(parameter), "Functional::previously has to return right result");
+        verify(mock, times(1)).run();
+    }
+
+    @Test
+    void withParamFunctionalTest() {
+        @SuppressWarnings("unchecked")
+        Function<Integer, Object> function = mock(Function.class);
+        when(function.apply(randomValue)).thenReturn(parameter);
+        verify(function, never()).apply(any());
+        assertSame(this.result, this.function.withParam(function::apply).apply(randomValue), "Functional::withParam(functional) has to return right result");
+        verify(function, times(1)).apply(randomValue);
+        verify(this.function, times(1)).apply(parameter);
+    }
+
+    @Test
+    void withParamSupplyableTest() {
+        @SuppressWarnings("unchecked")
+        Supplier<Object> supplier = mock(Supplier.class);
+        when(supplier.get()).thenReturn(parameter);
+        verify(supplier, never()).get();
+        assertSame(this.result, this.function.withParam(supplier::get).get(), "Functional::withParam(supplier) has to return right result");
+        verify(supplier, times(1)).get();
+        verify(this.function, times(1)).apply(parameter);
     }
 
     @Test
@@ -137,16 +205,24 @@ class FunctionalTest {
     }
 
     @BeforeEach
-    @SuppressWarnings("unchecked")
     void startUp() {
         this.parameter = mock();
         this.result = mock();
-        this.function = mock(Function.class);
-        when(function.apply(this.parameter)).thenReturn(this.result);
+        this.function = spy(new FunctionalImpl());
+        this.randomValue = random.nextInt();
     }
-
+    
+    private static final Random random = new Random();
     private Object parameter;
     private Object result;
-    private Function<Object, Object> function;
-
+    private Integer randomValue;
+    private Functional<Object, Object> function;
+    
+    class FunctionalImpl implements Functional<Object, Object> {
+        @Override
+        public Object execute(Object param) {
+            return result;
+        }
+    }
+    
 }
